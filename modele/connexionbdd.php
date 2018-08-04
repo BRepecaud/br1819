f507128d898a282ddb0ad3d4346f7ccff037c7ee
BRepecaud <benjamin.repecaud@orange.fr>
2018-08-04 11:57:37 +0200

    MAJ Modèle
    Connexion à la BDD, sélection de tous les projets.

--- modele/connexionbdd.php
+++ modele/connexionbdd.php
@@ -2,16 +2,17 @@
 //*******************************************
 //Connexion à la BDD
 //*******************************************
-
-try
+function connexion()
 {
-    
-    //-------------mysql:host=nomServeur; dbname=nomBDD; charset=utf8', 'nomUser', 'mdp');
-    $bdd = new PDO('mysql:host=localhost; dbname=br1819; charset=utf8', 'root', '');
+    try
+    {
+        //-------------mysql:host=nomServeur; dbname=nomBDD; charset=utf8', 'nomUser', 'mdp');
+        $bdd = new PDO('mysql:host=localhost; dbname=br1819_essai; charset=utf8', 'root', '');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur:'.$e.getMessage());
+    }
+    return $bdd;
 }
-catch(Exception $e)
-{
-    die('Erreur:'.$e.getMessage());
-}
-
 ?>
