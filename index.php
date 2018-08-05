60ee7470e941e85163b92444afdfc5d65a664fda
BRepecaud <benjamin.repecaud@orange.fr>
2018-08-05 13:42:19 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -7,47 +7,34 @@
     <body>
         <?php
 
-    //---------------------------------------MENU
-        require 'vue/vMenu.php';
-            
-    //---------------------------------------INCLUSION CONTROLEUR
-        /*
-            Soit descprojet, soit accueil
-         * vMenu: lien vers descprojet --> paramètre: action et nomprojet
-         * vérification de l'existance de ces paramètres
-         * si présent: require cDescprojet
-         * sinon: accueil
-        */
+        //---------------------------------------MENU
+            require 'vue/vMenu.php';
 
-            if(isset($_GET['action']))
-            {
-                if(isset($_GET['action']) == 'descprojet')
+        //---------------------------------------INCLUSION CONTROLEUR
+            /*
+                Soit descprojet, soit accueil
+             * vMenu: lien vers descprojet --> paramètre: action et nomprojet
+             * vérification de l'existance de ces paramètres
+             * si présent: require cDescprojet
+             * sinon: accueil
+            */
+
+                if(isset($_GET['action']))
                 {
-                    if(isset($_GET['nomprojet']))
+                    if(isset($_GET['action']) == 'descprojet')
                     {
-                        require('controleur/cDescprojet.php');
-                    }
-                    
-                }
-                
-            }
-            else
-            { 
-                /*
+                        if(isset($_GET['nomprojet']))
+                        {
+                            require('controleur/cDescprojet.php');
+                        }
 
-                 * 
-                 * 
-                 * 
-                 * CONTROLEUR GLOBAL
-                 * 
-                 * 
-                 * 
-                 *                  */
-                require 'controleur/cMain.php';
-                require 'controleur/cPresentation.php';
-                require 'controleur/cProjets.php';
-                require 'controleur/cContact.php';
-            }
+                    }
+
+                }
+                else
+                { 
+                    require 'controleur/cMain.php';                
+                }
         ?>
     </body>
 </html>
