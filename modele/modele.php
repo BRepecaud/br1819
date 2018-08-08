b5201d414c31d0c229e561c147baa467a4d75b43
BRepecaud <benjamin.repecaud@orange.fr>
2018-08-08 17:30:42 +0200

    Requêtes pour le carousel
    Count_image
    recup_image
    recup_video

--- modele/modele.php
+++ modele/modele.php
@@ -8,6 +8,7 @@ function connexion()
     try
     {
         //-------------mysql:host=nomServeur; dbname=nomBDD; charset=utf8', 'nomUser', 'mdp');
+        
         return $bdd;
     }
     catch(Exception $e)
@@ -51,12 +52,59 @@ function recup_desc()
 {
     $bdd = connexion();
     
+    //WHERE nomprojet = ? --> ? correspond au nom de projet passé en paramètre dans le lien vers le projet en question.
     $requete_sql = $bdd->prepare("SELECT * FROM projet WHERE nomprojet = ?");
     $requete_sql->execute(array($_GET['nomprojet']));
     
     $desc = $requete_sql->fetch();
-    
     return $desc;
+}
+
+/*
+    COMPTE LE NOMBRE D'IMAGE DES PROJETS
+ * (utile pour le carousel)
+*/
+function count_image()
+{
+    $bdd = connexion();
+    
+    $requete_sql = $bdd->prepare('SELECT COUNT(lien_image) FROM carousel WHERE nomprojet_carousel = ?');
+    $requete_sql->execute(array($_GET['nomprojet']));
+    
+    $nombreImage = $requete_sql->fetch();
+    
+    //-----------------$nombreImage[0]: contenu du resultat = nombre d'image
+    return ($nombreImage[0]);
+}
+
+/*
+    RECUP DES IMAGES DES PROJETS
+ * selection des éléments de la table carousel comparés avec le nom du projet sélectionné
+*/
+function recup_image()
+{
+    $bdd = connexion();
+  
+    $requete_sql = $bdd->prepare('SELECT * FROM carousel WHERE nomprojet_carousel = ?');
+    $requete_sql->execute(array($_GET['nomprojet']));
+
+    $img = $requete_sql->fetchAll();
+    return $img;
+}
+
+/*
+    RECUP DES IMAGES DES PROJETS
+ * selection des éléments de la table carousel comparés avec le nom du projet sélectionné
+*/
+function recup_video()
+{
+    $bdd = connexion();
+  
+    $requete_sql = $bdd->prepare('SELECT * FROM carousel WHERE nomprojet_carousel = ?');
+    $requete_sql->execute(array($_GET['nomprojet']));
+
+    $video = $requete_sql->fetch();
+    return $video;
 }
 
 ?>
