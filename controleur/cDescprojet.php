51c64b75bd6baf5ea41db6e367c6423eb758b573
BRepecaud <benjamin.repecaud@orange.fr>
2018-08-08 17:29:23 +0200

    MAJ Constructeur

--- controleur/cDescprojet.php
+++ controleur/cDescprojet.php
@@ -2,5 +2,8 @@
 //------------CONTROLEUR
     require 'modele/modele.php';
     $desc = recup_desc();
+    $nombreImage = count_image();
+    $img = recup_image();
+    $video = recup_video();
     require 'vue/vDescprojet.php';
 ?>
